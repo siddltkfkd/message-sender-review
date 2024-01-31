b1dda415cf052f4eae40f641650a0b442bc88fb3
siddltkfkd <unji2000@gmail.com>
2024-01-31 20:55:48 +0900

    필드 주입으로 의존성 주입(config)

---
+++ src/main/java/com/nhnacademy/edu/springframework/config/javaConfig.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.edu.springframework.config;
+
+import com.nhnacademy.edu.springframework.sender.EmailMessageSender;
+import com.nhnacademy.edu.springframework.sender.MessageSender;
+import com.nhnacademy.edu.springframework.sender.SmsMessageSender;
+import com.nhnacademy.edu.springframework.service.MessageSendService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+public class javaConfig {
+	@Bean
+	public MessageSender emailMessageSender(){
+		return new EmailMessageSender();
+	}
+	@Bean
+	public MessageSender smsMessageSender(){
+		return new SmsMessageSender();
+	}
+	@Bean
+	public MessageSendService messageSendService(){
+		return new MessageSendService();
+	}
+}
